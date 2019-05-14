3c3d7733ef4a2a290ac5c68101daba1d85fd5e07
Sssssunny <hhe330@naver.com>
2019-05-14 16:25:44 +0900

    오늘의 운세

--- lucky.js
+++ lucky.js
@@ -7,12 +7,14 @@ const port = 3000;
 
 app.get('/', function(req,res){
     
+    // 슬랙 채널
     const webhookUri = "https://hooks.slack.com/services/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";
     const slack = new Slack();
     slack.setWebhook(webhookUri);
 
     let options = {
         method: 'GET',
+        // 오늘의 운세 링크
         uri: 'https://m.search.naver.com/p/csearch/dcontent/external_api/json_todayunse_v2.naver?_callback=window.__jindo2_callback._fortune_my_0&gender=f&birth=20021022&solarCal=solar&time=',
         headers: {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
@@ -25,22 +27,20 @@ app.get('/', function(req,res){
 
             const retData = body.replace('window.__jindo2_callback._fortune_my_0(','').replace(');','').replace(/\s([A-z]+)\s?:/g,'"$1":').replace('\n','').replace('<b>' ,'*').replace('</b>', '*');
             const jsonData =  JSON.parse(retData);
-            const result = JSON.stringify(retData);
 
             res.send(jsonData.result.day.content[0].desc);
-
-            console.log('--------------------------------------');
+            
+            console.log('-------------------총운-------------------');
             console.log(jsonData.result.day.content[0].keyword);
             console.log();
             console.log(jsonData.result.day.content[0].desc);
             console.log('--------------------------------------');                      
 
             slack.webhook({
-                channel: "2019_도제학생방",      //전송될 슬랙 채널
-                username: "오늘의 운세",  // 슬랙에 표시될 이름
+                channel: "2019_도제학생방", //슬랙 채널
+                username: "써니의 총운",  // 슬랙봇 이름
                 icon_emoji: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqXX85E3HssrkWI83XT1uPQ8ds0_9AAkzgvJ5o3gf242o9HghOSA',
-                text: jsonData.result.day.content[0].keyword + '\n' +
-                      '>>>' + jsonData.result.day.content[0].desc
+                text: `${jsonData.result.day.content[0].keyword}\n>>>${jsonData.result.day.content[0].desc}`
               }, function(err, response){
                   console.log("===========webhook res===============");
                   console.log(response);
